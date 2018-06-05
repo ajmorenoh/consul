5d8aa668e36d134bb675679a64b8b0c4168f6de1
iagirre <itziar.agirre@m2c.es>
2018-06-05 11:43:16 +0200

    Add translations management pages
    In the admin section of the application, a new page
    has been added so that the admins are able to manage
    the selected texts for translate.
    
    The texts have been divided in different "sections",
    depending on the nature of themselves (budgets, polls,
    proposals, management, etc.). Each section has become a tab
    with a form associated to edit all the texts for her.
    
    When a language is added, it's added for ALL the texts in the
    application. That means that, if an admin adds french for debates,
    the french form will appear for the rest of the texts. That doesn't
    mean that they need to fill all the texts, only that the languages
    work for all of them instead of individually.

--- app/models/i18n_content.rb
+++ app/models/i18n_content.rb
@@ -3,4 +3,17 @@ class I18nContent < ActiveRecord::Base
 
   translates :value, touch: true
   globalize_accessors locales: [:en, :es, :fr, :nl, :val, :pt_br]
+
+  scope :debates,           -> { where("key like ?", "debates.%")}
+  scope :emails,            -> { where("key like ?", "mailers.%")}
+  scope :management,        -> { where("key like ?", "management.%")}
+  scope :guides,            -> { where("key like ?", "guides.%")}
+  scope :devise_locales,    -> { where("key like ?", "devise.%")}
+  scope :devise_views,      -> { where("key like ?", "devise_views.%")}
+  scope :layouts,           -> { where("key like ?", "layouts.%")}
+  scope :legislation,       -> { where("key like ?", "legislation.%")}
+  scope :budgets,           -> { where("key like ?", "budgets.%")}
+  scope :polls,             -> { where("key like ?", "polls.%")}
+  scope :proposals,         -> { where("key like ?", "proposals.%")}
+  scope :community,         -> { where("key like ?", "community.%")}
 end

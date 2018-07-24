1b1bc3c8a02bebf2174279f27b2d6eeb24806546
María Checa <MariaCheca@users.noreply.github.com>
2018-07-24 19:42:19 +0200

    Add total Investments in the execution list

--- app/helpers/budget_executions_helper.rb
+++ app/helpers/budget_executions_helper.rb
@@ -3,4 +3,11 @@ module BudgetExecutionsHelper
   def spending_proposals?
     @budget.slug == '2016'
   end
+
+  def filters_select_counts(status)
+    @budget.investments.winners.with_milestones.select { |i| i.milestones
+                                      .published.with_status.order_by_publication_date
+                                      .try(:last).try(:status_id) == status }.count
+  end
+
 end

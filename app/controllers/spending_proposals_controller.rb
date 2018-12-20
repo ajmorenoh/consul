f44a0c52e420a8fe0a16471cdae685d7107ef650
voodoorai2000 <voodoorai2000@gmail.com>
2018-12-20 13:45:09 +0100

    Update spending_proposals_controller.rb
    Now that we have migrated delegated votes to represented user votes[1] we should not add represented users(`delegators`) to participants.
    
    As their participation has already been counted as `voters` with their migrated votes.
    
    Specs have been broken for a while[2]. Hard to test. Manual testing in the staging server should suffice.
    
    [1] https://github.com/AyuntamientoMadrid/consul/pull/1770
    
    [2] https://github.com/AyuntamientoMadrid/consul/blob/master/spec/features/spending_proposals_spec.rb#L641

--- app/controllers/spending_proposals_controller.rb
+++ app/controllers/spending_proposals_controller.rb
@@ -178,7 +178,7 @@ class SpendingProposalsController < ApplicationController
 
     def participants
       stats_cache('participants') do
-        users = (authors + voters + balloters + delegators).uniq
+        users = (authors + voters + balloters).uniq
         User.where(id: users)
       end
     end
@@ -231,10 +231,6 @@ class SpendingProposalsController < ApplicationController
       stats_cache("balloters_geozone_#{geozone_id}") do
         Ballot.where('ballot_lines_count > ? AND geozone_id = ?', 0, geozone_id).pluck(:user_id)
       end
-    end
-
-    def delegators
-      stats_cache('delegators') { User.where.not(representative_id: nil).pluck(:id) }
     end
 
     def total_spending_proposals

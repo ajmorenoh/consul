e73613c4ad2cdfe3c2116ffb93f623f358e0f3be
Julian Herrero <microweb10@gmail.com>
2019-01-22 18:18:24 +0100

    Make budget headings translatable

--- app/models/budget/heading.rb
+++ app/models/budget/heading.rb
@@ -4,13 +4,16 @@ class Budget
 
     include Sluggable
 
+    translates :name, touch: true
+    include Globalizable
+
     belongs_to :group
 
     has_many :investments
     has_many :content_blocks
 
+    validates_translation :name, presence: true
     validates :group_id, presence: true
-    validates :name, presence: true, uniqueness: { if: :name_exists_in_budget_headings }
     validates :price, presence: true
     validates :slug, presence: true, format: /\A[a-z0-9\-_]+\z/
     validates :population, numericality: { greater_than: 0 }, allow_nil: true
@@ -19,23 +22,31 @@ class Budget
     validates :longitude, length: { maximum: 22 }, allow_blank: true, \
               format: /\A(-|\+)?((?:1[0-7]|[1-9])?\d(?:\.\d{1,})?|180(?:\.0{1,})?)\z/
 
+    validate :name_uniqueness_by_budget
+
     delegate :budget, :budget_id, to: :group, allow_nil: true
 
     scope :order_by_group_name, -> do
-      joins(group: :translations).order("budget_group_translations.name DESC", "budget_headings.name")
+      includes(:translations)
+        .joins(group: :translations)
+        .order("budget_group_translations.name DESC", "budget_heading_translations.name")
     end
-    scope :allow_custom_content, -> { where(allow_custom_content: true).order(:name) }
+    scope :allow_custom_content, -> do
+      includes(:translations).where(allow_custom_content: true).order(:name)
+    end
 
     def name_scoped_by_group
       group.single_heading_group? ? name : "#{group.name}: #{name}"
     end
 
     def to_param
-      name.parameterize
+      slug
     end
 
-    def name_exists_in_budget_headings
-      group.budget.headings.where(name: name).where.not(id: id).any?
+    def name_uniqueness_by_budget
+      if group.budget.headings.includes(:translations).where(name: name).where.not(id: id).any?
+        errors.add(:name, I18n.t("errors.messages.taken"))
+      end
     end
 
     def can_be_deleted?

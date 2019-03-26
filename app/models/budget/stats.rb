cc51571e2aace3955539788233e6e95cc75f9703
Javi Martín <javim@elretirao.net>
2019-03-26 14:26:27 +0100

    Don't count `nil` as balloter

--- app/models/budget/stats.rb
+++ app/models/budget/stats.rb
@@ -115,7 +115,7 @@ class Budget::Stats
     end
 
     def balloters
-      budget.ballots.where("ballot_lines_count > ?", 0).distinct.pluck(:user_id)
+      budget.ballots.where("ballot_lines_count > ?", 0).distinct.pluck(:user_id).compact
     end
 
     def poll_ballot_voters

consuldemocracy/consuldemocracy

removes controller spec deprecation warnings

spec/controllers/pages_controller_spec.rb
@@ -4,63 +4,63 @@ describe PagesController do
 
   describe 'Static pages' do
     it 'should include a privacy page' do
-      get :show, id: :privacy
+      get :show, params: { id: :privacy }
       expect(response).to be_ok
     end
 
     it 'should include a conditions page' do
-      get :show, id: :conditions
+      get :show, params: { id: :conditions }
       expect(response).to be_ok
     end
 
     it 'should include a general terms page' do
-      get :show, id: :general_terms
+      get :show, params: { id: :general_terms }
       expect(response).to be_ok
     end
 
     it 'should include a terms page' do
-      get :show, id: :census_terms
+      get :show, params: { id: :census_terms }
       expect(response).to be_ok
     end
   end
 
   describe 'Provisional pages' do
     it 'should include a opendata page' do
-      get :show, id: :opendata
+      get :show, params: { id: :opendata }
       expect(response).to be_ok
     end
   end
 
   describe 'Info pages' do
     it 'should include a how_it_works page' do
-      get :show, id: :how_it_works
+      get :show, params: { id: :how_it_works }
       expect(response).to be_ok
     end
 
     it 'should include a how_to_use page' do
-      get :show, id: :how_to_use
+      get :show, params: { id: :how_to_use }
       expect(response).to be_ok
     end
 
     it 'should include a more_information page' do
-      get :show, id: :more_information
+      get :show, params: { id: :more_information }
       expect(response).to be_ok
     end
 
     it 'should include a participation page' do
-      get :show, id: :participation
+      get :show, params: { id: :participation }
       expect(response).to be_ok
     end
 
     it 'should include a accessibility page' do
-      get :show, id: :accessibility
+      get :show, params: { id: :accessibility }
       expect(response).to be_ok
     end
   end
 
   describe 'Not found pages' do
     it 'should return a 404 message' do
-      get :show, id: "nonExistentPage"
+      get :show, params: { id: "nonExistentPage" }
       expect(response).to be_missing
     end
   end

ajmorenoh/consul

spec/controllers/pages_controller_spec.rb
@@ -4,22 +4,22 @@ describe PagesController do
 
   describe "Static pages" do
     it "includes a privacy page" do
-      get :show, id: :privacy
+      get :show, params: { id: :privacy }
       expect(response).to be_ok
     end
 
     it "includes a conditions page" do
-      get :show, id: :conditions
+      get :show, params: { id: :conditions }
       expect(response).to be_ok
     end
 
     it "includes a accessibility page" do
-      get :show, id: :accessibility
+      get :show, params: { id: :accessibility }
       expect(response).to be_ok
     end
 
     it "includes a custom census terms page" do
-      get :show, id: :census_terms
+      get :show, params: { id: :census_terms }
       expect(response).to be_ok
     end
   end
@@ -27,24 +27,24 @@ describe PagesController do
   describe "More info pages" do
 
     it "includes a more info page" do
-      get :show, id: "help/index"
+      get :show, params: { id: "help/index" }
       expect(response).to be_ok
     end
 
     it "includes a how_to_use page" do
-      get :show, id: "help/how_to_use/index"
+      get :show, params: { id: "help/how_to_use/index" }
       expect(response).to be_ok
     end
 
     it "includes a faq page" do
-      get :show, id: :faq
+      get :show, params: { id: :faq }
       expect(response).to be_ok
     end
   end
 
   describe "Not found pages" do
     it "returns a 404 message" do
-      get :show, id: "nonExistentPage"
+      get :show, params: { id: "nonExistentPage" }
       expect(response).to be_missing
     end
   end